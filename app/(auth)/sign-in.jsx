14e9cf7f12ab5ead8735d206378bcefbc748c5d8
Jayaprakash Sahoo <aryakumarjayaprakash@gmail.com>
2024-05-04 19:40:23 +0530

    auth screens done

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,23 +1,61 @@
-import { View, Text, ScrollView, Image } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../../constants'
-import FormField from '../../components/FormField'
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import { Link } from "expo-router";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const signin = () => {};
+
   return (
-    <SafeAreaView className='bg-primary h-full'>
+    <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className='w-full min-h-[85vh] my-6 px-4'>
-          <View className=' flex flex-row gap-2'>
-            <Image source={images.logoSmall} className=' w-[35px] h-[40px]' resizeMode='contain' />
-            <Text className=' font-pbold text-white text-4xl'>Vide</Text>
+        <View className="w-full min-h-[85vh] my-6 px-4">
+          <View className=" flex flex-row gap-2">
+            <Image
+              source={images.logoSmall}
+              className=" w-[35px] h-[40px]"
+              resizeMode="contain"
+            />
+            <Text className=" font-pbold text-white text-4xl">Vide</Text>
           </View>
-          <Text className='text-white text-2xl mt-4 font-psemibold'>Login to Vide</Text>
+          <Text className="text-white text-2xl mt-4 font-psemibold">
+            Login to Vide
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChange={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChange={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign in"
+            handlePress={signin}
+            containerStyles="w-full mt-7"
+          />
+          <View className="justify-center w-full flex-row gap-2 pt-5">
+            <Text className="text-gray-200">Don't have an account?</Text>
+            <Link className="text-secondary font-psemibold" href={"/sign-up"}>
+              Sign up
+            </Link>
+          </View>
         </View>
-        <FormField />
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,12 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import { Link } from "expo-router";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
 const SignUp = () => {
-  return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
-  )
-}
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const signup = () => {};
 
-export default SignUp
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full min-h-[85vh] my-6 px-4">
+          <View className=" flex flex-row gap-2">
+            <Image
+              source={images.logoSmall}
+              className=" w-[35px] h-[40px]"
+              resizeMode="contain"
+            />
+            <Text className=" font-pbold text-white text-4xl">Vide</Text>
+          </View>
+          <Text className="text-white text-2xl mt-4 font-psemibold">
+            Sign up to Vide
+          </Text>
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChange={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-7"
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChange={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChange={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign up"
+            handlePress={signup}
+            containerStyles="w-full mt-7"
+          />
+          <View className="justify-center w-full flex-row gap-2 pt-5">
+            <Text className="text-gray-200">Already have an account?</Text>
+            <Link className="text-secondary font-psemibold" href={"/sign-in"}>
+              Sign in
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
